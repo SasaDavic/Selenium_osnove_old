56fb8cf109e2aad7fa147d41cc20b34091233fd2
Sasa Davic <sasadavic.nis@gmail.com>
2025-02-15 19:51:02 +0100

    Osnovna podesavanja

---
+++ src/p09_09_2022/uvodUTestNg.java
@@ -0,0 +1,12 @@
+package p09_09_2022;
+
+import org.testng.annotations.Test;
+
+public class uvodUTestNg {
+
+	
+	@Test
+	public void test1() {
+		System.out.println("Test 1");
+	}
+}
---
+++ src/p09_09_2022/zadatak.java
@@ -0,0 +1,24 @@
+package p09_09_2022;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class zadatak {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();   
+		driver.manage().window().maximize();	//max prozor
+		driver.get("https://cms.demo.katalon.com/");	//Ucitava stranicu
+		WebElement searshInput = driver.findElement(By.xpath("//input[@name='s']")); //deo za pretragu
+													//By.name("s")
+		searshInput.sendKeys("Flying Ninja");	//unosi tekst Flying Ninja
+		
+		Thread.sleep(10000);	//Ceka 10 sekuni
+		driver.quit();	//Zatvara pretrazivac
+
+	}
+
+}
