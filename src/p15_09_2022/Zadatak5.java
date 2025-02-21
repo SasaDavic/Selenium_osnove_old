330189da30b5389ad659aff778158c359a7c2eac
Sasa Davic <sasadavic.nis@gmail.com>
2025-02-21 20:00:29 +0100

    predavanja

--- src/p15_09_2022/Zadatak5.java
+++ src/p15_09_2022/Zadatak5.java
@@ -2,8 +2,11 @@ package p15_09_2022;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class Zadatak5 {
 
@@ -26,7 +29,36 @@ Gasi stranicu
 		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-		driver.get("https://demoqa.com/login"); // ne moze da ucita kasnije...
+		driver.get("https://practice.expandtesting.com/login"); 
+		String username = driver.findElement(By.xpath("//div[contains(@class, 'col-9')]/ul/li/b")).getText();
+		String pass = driver.findElement(By.xpath("//div[contains(@class, 'col-9')]/ul/li[2]/b")).getText();
+		
+		driver.findElement(By.id("username")).sendKeys(username);
+		driver.findElement(By.id("password")).sendKeys(pass);
+		
+		 WebElement buttonLogin = driver.findElement(By.id("login")).findElement(By.tagName("button")); //odskorlajte do login dugmeta
+	        new Actions(driver)
+	                .moveToElement(buttonLogin)
+	                .perform();
+	        
+	        driver.findElement(By.id("login")).findElement(By.tagName("button")).click();
+		
+		boolean elementExist = true;
+		try {
+			driver.findElement(By.xpath("//div[contains(@class, 'container')]/a"));
+		} catch (Exception e) {
+			elementExist = false;
+		}
+		
+		if (elementExist) {
+			System.out.println("Korisnik se uspesno ulogovao!");
+			driver.findElement(By.xpath("//div[contains(@class, 'container')]/a")).click();
+		} else {
+			System.out.println("Korisnik se nije ulogovao!");
+		}
+		
+		
+		
 		
 		Thread.sleep(5000);
 		driver.quit();
--- src/p15_09_2022/Zadatak6.java
+++ src/p15_09_2022/Zadatak6.java
@@ -2,6 +2,7 @@ package p15_09_2022;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -22,7 +23,23 @@ Proverite da li se prikazao dijalog i ispisite u konzoli odgovarajuce poruke
 		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-		driver.get("https://demoqa.com/modal-dialogs");// ne moze da ucita kasnije...
+		driver.get("https://demoqa.com/modal-dialogs");
+	//	driver.findElement(By.id("showLargeModal")).click();
+		boolean elementExist = true;
+		
+		try {
+			driver.findElement(By.className("modal-content")).isDisplayed();
+		} catch (Exception e) {
+			elementExist = false;
+		}
+				
+		
+		if (elementExist) {
+			System.out.println("Large modal is displayed!");
+		} else {
+			System.out.println("Large modal is not displayed!");
+		}
+		
 		
 		Thread.sleep(2000);
 		driver.quit();
